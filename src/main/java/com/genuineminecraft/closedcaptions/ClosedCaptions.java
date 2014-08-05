4c8890e9023f3960f8aa2b8d40cac9f7510fbc8c
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2014-08-04 19:25:32 -0700

    Versioning update

--- src/main/java/com/genuineminecraft/closedcaptions/ClosedCaptions.java
+++ src/main/java/com/genuineminecraft/closedcaptions/ClosedCaptions.java
@@ -31,7 +31,7 @@ public class ClosedCaptions {
 
 	public static final String MODID = "ClosedCaptions";
 	public static final String NAME = "[Genuine] ClosedCaptions";
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.6";
 	@Instance(ClosedCaptions.MODID)
 	public static ClosedCaptions instance;
 	private File folder;
