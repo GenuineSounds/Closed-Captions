d69167791770ef52d77c7cad74a50d6450b26b28
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2014-08-08 00:55:18 -0700

    Fixing lame stuff

--- src/main/java/com/genuineminecraft/closedcaptions/ClosedCaptions.java
+++ src/main/java/com/genuineminecraft/closedcaptions/ClosedCaptions.java
@@ -29,7 +29,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class ClosedCaptions {
 
 	public static final String MODID = "ClosedCaptions";
-	public static final String NAME = "[Genuine] ClosedCaptions";
+	public static final String NAME = "[Genuine] Closed Captions";
 	public static final String VERSION = "1.6";
 	@Instance(ClosedCaptions.MODID)
 	public static ClosedCaptions instance;
